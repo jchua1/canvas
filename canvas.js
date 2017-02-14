10fa9dfc1a82848507716fdf796e193055b710ff
jchua1 <jchua@stuy.edu>
2017-02-14 02:04:52 -0500

    done

--- canvas.js
+++ canvas.js
@@ -1,5 +1,15 @@
 var c = document.getElementById('canvas');
 var ctx = c.getContext('2d');
+var b = document.getElementById('button');
 
-ctx.fillStyle = '##ff0000';
-ctx.fillRect(50, 50, 100, 200);
+ctx.fillStyle = 'red';
+
+c.addEventListener('click', function(event) {
+    var x = event.offsetX;
+    var y = event.offsetY;
+    ctx.fillRect(x, y, 50, 100);
+});
+
+b.addEventListener('click', function() {
+    ctx.clearRect(0, 0, 500, 500);
+});
