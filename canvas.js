afc1ae490001090b8b237637e838e93b9e6d877f
jchua1 <jchua@stuy.edu>
2017-02-15 01:10:37 -0500

    done with dot connecting

--- canvas.js
+++ canvas.js
@@ -5,32 +5,44 @@ var b = document.getElementById('button');
 ctx.fillStyle = 'red';
 
 var clear = function() {
+    ctx.closePath();
     ctx.clearRect(0, 0, 500, 500);
-    ctx.closePath()
     begin = true;
-};
+}
 
 var rect = function(e) {
     var x = e.offsetX;
     var y = e.offsetY;
     ctx.fillRect(x, y, 50, 100);
-};
+}
 
 var begin = true;
-ctx.beginPath();
-ctx.moveTo(0, 0);
 
 var dots = function(e) {
     var x = e.offsetX;
     var y = e.offsetY;
     if (begin) {
-	ctx.moveTo(x, y)
+	ctx.beginPath();
 	begin = false;
     }
-    ctx.arc(x, y, 5, 0, 2*Math.PI);
     ctx.lineTo(x, y);
     ctx.stroke();
-};
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.moveTo(x, y);
+}  
+    
+var circle = function(e) {
+    ctx.beginPath();
+    var x = e.offsetX;
+    var y = e.offsetY;
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.moveTo(x, y);
+    ctx.closePath();
+}
 
 c.addEventListener('click', dots);
 
