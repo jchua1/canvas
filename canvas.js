336f2b5497cafb48f8e9bbc5a265978c764cf140
jchua1 <jchua@stuy.edu>
2017-02-17 01:31:08 -0500

    update

--- canvas.js
+++ canvas.js
@@ -1,6 +1,8 @@
 var c = document.getElementById('canvas');
 var ctx = c.getContext('2d');
 var b = document.getElementById('button');
+var vup = document.getElementById('vup');
+var vdown = document.getElementById('vdown');
 
 ctx.fillStyle = 'red';
 
@@ -45,27 +47,32 @@ var circle = function(e) {
 };
 
 var requestID;
+var movement = 'right'
+var increment = 1
 
 var animate = function() {
 
     window.cancelAnimationFrame(requestID);
     
     var radius = 50;
-    var xcor = 10;
+    var xcor = 250;
+    var ycor = 250;
 
     var drawDot = function() {
-	console.log(requestID);
+	//console.log(requestID);
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.beginPath();
-	ctx.arc(xcor, 100, radius, 0, 2 * Math.PI);
+	ctx.arc(xcor, ycor, radius, 0, 2 * Math.PI);
 	ctx.stroke();
 	ctx.fill();
-	if movement == 'up' {
-	    ycor++;
-	};
-	else if movement == 'down' {
-	    ycor--;
-	};
+	if (movement == 'right')
+	    xcor += increment;
+	else if (movement == 'left')
+	    xcor -= increment;
+	else if (movement == 'up')
+	    ycor -= increment;
+	else if (movement == 'down')
+	    ycor += increment;
 	requestID = window.requestAnimationFrame(drawDot);
     };
 
@@ -73,10 +80,42 @@ var animate = function() {
 };
 
 var stop = function() {
-    console.log(requestID);
+    //console.log(requestID);
     window.cancelAnimationFrame(requestID);
 };
+
+var change = function(e) {
+    var keyCode = e.keyCode;
+    if (keyCode == 87)
+	movement = 'up';
+    else if (keyCode == 83)
+	movement = 'down';
+    else if (keyCode == 65)
+	movement = 'left';
+    else if (keyCode == 68)
+	movement = 'right';
+};
+
+var fast = function() {
+    if (increment < 5)
+	increment++;
+    else
+	console.log("Maximum velocity reached");
+};
+
+var slow = function() {
+    if (increment > 1)
+	increment--;
+    else
+	console.log("Minimum velocity reached");
+};  
 
 c.addEventListener('click', animate);
 
 b.addEventListener('click', stop);
+
+document.addEventListener('keydown', change);
+
+vup.addEventListener('click', fast);
+
+vdown.addEventListener('click', slow);
