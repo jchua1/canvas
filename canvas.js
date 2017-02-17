f2da5244320456f02a15049278e562cf28527f32
jchua1 <jchua@stuy.edu>
2017-02-17 14:58:12 -0500

    update

--- canvas.js
+++ canvas.js
@@ -3,6 +3,9 @@ var ctx = c.getContext('2d');
 var b = document.getElementById('button');
 var vup = document.getElementById('vup');
 var vdown = document.getElementById('vdown');
+var gc = document.getElementById('circle');
+var d = document.getElementById('dvd');
+var s = document.getElementById('stop');
 
 ctx.fillStyle = 'red';
 
@@ -12,59 +15,33 @@ var clear = function() {
     begin = true;
 };
 
-var rect = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    ctx.fillRect(x, y, 50, 100);
-};
-
-var begin = true;
-
-var dots = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    if (begin) {
-	ctx.beginPath();
-	begin = false;
-    };
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.moveTo(x, y);
-};
-    
-var circle = function(e) {
-    ctx.beginPath();
-    var x = e.offsetX;
-    var y = e.offsetY;
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-    ctx.moveTo(x, y);
-    ctx.closePath();
-};
-
 var requestID;
-var movement = 'right'
-var increment = 1
+var movement = 'right';
+var increment = 1;
+var img = new Image();
+img.src = 'dvd.png';
 
 var animate = function() {
 
     window.cancelAnimationFrame(requestID);
+    console.log(img.width);
     
     var radius = 50;
-    var xcor = 250;
+    var xcor = 100;
     var ycor = 250;
+    var xinc = 1;
+    var yinc = 1;
 
     var drawDot = function() {
 	//console.log(requestID);
+
 	ctx.clearRect(0, 0, c.width, c.height);
+	/*
 	ctx.beginPath();
 	ctx.arc(xcor, ycor, radius, 0, 2 * Math.PI);
 	ctx.stroke();
 	ctx.fill();
+	
 	if (movement == 'right')
 	    xcor += increment;
 	else if (movement == 'left')
@@ -73,14 +50,60 @@ var animate = function() {
 	    ycor -= increment;
 	else if (movement == 'down')
 	    ycor += increment;
+	*/
+
+	ctx.drawImage(img, xcor, ycor);
+	xcor += xinc;
+	ycor += yinc;
+	console.log(xcor + img.naturalWidth);
+	if (xcor + img.naturalWidth == c.width)
+	    xinc = -1;
+	if (ycor + img.naturalHeight == c.height)
+	    yinc = -1;
+	if (xcor == 0)
+	    xinc = 1;
+	if (ycor == 0)
+	    yinc = 1;
+
 	requestID = window.requestAnimationFrame(drawDot);
     };
 
     drawDot();
 };
 
+var growingCircle = function() {
+
+    var radius = 50;
+    var xcor = 250;
+    var ycor = 250;
+    var increment = 1;
+
+    var draw = function() {
+	
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
+	ctx.arc(xcor, ycor, radius, 0, 2 * Math.PI);
+	ctx.stroke();
+	ctx.fill();
+
+	if (increment == 1) {
+	    radius++;
+	    if (xcor + radius == c.width)
+		increment = -1;
+	}
+	else {
+	    radius--;
+	    if (radius == 0)
+		increment = 1;
+	}
+
+	requestID = window.requestAnimationFrame(draw);
+    };
+
+    draw();
+};
+
 var stop = function() {
-    //console.log(requestID);
     window.cancelAnimationFrame(requestID);
 };
 
@@ -110,12 +133,15 @@ var slow = function() {
 	console.log("Minimum velocity reached");
 };  
 
-c.addEventListener('click', animate);
+d.addEventListener('click', animate);
 
-b.addEventListener('click', stop);
+gc.addEventListener('click', growingCircle);
 
+s.addEventListener('click', stop)
+/*
 document.addEventListener('keydown', change);
 
 vup.addEventListener('click', fast);
 
 vdown.addEventListener('click', slow);
+*/
